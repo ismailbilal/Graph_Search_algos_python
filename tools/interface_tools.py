846adc28b3dacfabec9e1eedaf24b907239b314d
ismailbilal <is123bilal@gmail.com>
2022-12-31 13:46:26 +0100

    add this file where all interface aider functions will be included

---
+++ tools/interface_tools.py
@@ -0,0 +1,8 @@
+def show_graph_algos(boolean_variable, ui):
+    ui.algos_arbre.hide()
+    ui.algos_graph.show()
+
+
+def show_arbre_algos(boolean_variable, ui):
+    ui.algos_graph.hide()
+    ui.algos_arbre.show()
